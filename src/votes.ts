7692b1ce283862e97e8bf7773f774e0107c448ae
Nikhil Ranjan <niklabh811@gmail.com>
2021-06-02 16:28:35 +0530

    fix

--- src/votes.ts
+++ src/votes.ts
@@ -1,14 +1,23 @@
 const { prisma } = require('./generated/prisma-client');
 
+const NETWORK = process.env.NETWORK;
+const REFRENDUM_INDEX = process.env.REFRENDUM_INDEX;
+const MOTION_INDEX = process.env.MOTION_INDEX;
+
+if (!NETWORK || !Number(REFRENDUM_INDEX) || !Number(MOTION_INDEX)) {
+	console.error("Please provide NETWORK and REFRENDUM_INDEX and MOTION_INDEX environment variables");
+	process.exit(1);
+}
+
 const wait = () => new Promise(r => {
 	setTimeout(r, 1000)
 });
 
 async function start() {
 
-	console.log('start');
+	console.log('getting votes');
 
-	for (let i = 0; i <= 24; i++) {
+	for (let i = 0; i <= Number(REFRENDUM_INDEX); i++) {
 		console.log(`processing ${i}`)
 		await wait();
 
@@ -18,7 +27,7 @@ async function start() {
 			"referendum_index": i
 		};
 
-		const result = await fetch("https://polkadot.subscan.io/api/scan/democracy/votes", {
+		const result = await fetch(`https://${NETWORK}.subscan.io/api/scan/democracy/votes`, {
 			"headers": {
 				"accept": "application/json, text/plain, */*",
 				"accept-language": "en",
@@ -30,7 +39,7 @@ async function start() {
 				"sec-fetch-site": "same-origin",
 				"cookie": "_ga=GA1.2.373893696.1615810717; __gads=ID=719a8ea82f697fe1-22f2d006c8ba0095:T=1615810718:RT=1615810718:S=ALNI_Maikj6BaNzar0UggfrRoUNB-4cQzg; banner=true; __cfduid=d729b35ad5eae5a6c9af7f724dccd64191618817702; local_language=en; _gid=GA1.2.943938073.1618899540; _gat_UA1525613143=1; _gat_UA1525613147=1"
 			},
-			"referrer": `https://polkadot.subscan.io/referenda_vote/${i}`,
+			"referrer": `https://${NETWORK}.subscan.io/referenda_vote/${i}`,
 			"referrerPolicy": "origin-when-cross-origin",
 			"body": JSON.stringify(body),
 			"method": "POST",
@@ -56,6 +65,53 @@ async function start() {
 			})
 		}
 	}
+
+	console.log('getting motions');
+
+	for (let i = 0; i <= Number(MOTION_INDEX); i++) {
+		console.log(`processing ${i}`)
+		await wait();
+
+		const body = {
+			"proposal_id":i
+		};
+
+		const result = await fetch(`https://${NETWORK}.subscan.io/api/scan/council/proposal`, {
+			"headers": {
+			  "accept": "application/json, text/plain, */*",
+			  "accept-language": "en",
+			  "content-type": "application/json;charset=UTF-8",
+			  "sec-ch-ua": "\"Google Chrome\";v=\"89\", \"Chromium\";v=\"89\", \";Not A Brand\";v=\"99\"",
+			  "sec-ch-ua-mobile": "?0",
+			  "sec-fetch-dest": "empty",
+			  "sec-fetch-mode": "cors",
+			  "sec-fetch-site": "same-origin",
+			  "cookie": "_ga=GA1.2.373893696.1615810717; __gads=ID=719a8ea82f697fe1-22f2d006c8ba0095:T=1615810718:RT=1615810718:S=ALNI_Maikj6BaNzar0UggfrRoUNB-4cQzg; banner=true; __cfduid=d729b35ad5eae5a6c9af7f724dccd64191618817702; local_language=en; _gid=GA1.2.943938073.1618899540; _gat_UA1525613143=1; _gat_UA1525613147=1"
+			},
+			"referrer": `https://${NETWORK}.subscan.io/council/${i}?tab=votes`,
+			"referrerPolicy": "origin-when-cross-origin",
+			"body": JSON.stringify(body),
+			"method": "POST",
+			"mode": "cors"
+		});
+
+		const json = await result.json();
+
+		if (json.data?.info?.votes?.length) {
+			json.data?.info?.votes.forEach(async (row: any) => {
+				const r = await prisma.createVote({
+					proposalType: 'motion',
+					proposalId: i,
+					address: row.account?.address,
+					addressDisplay: row.account?.display,
+					vote: row.passed ? 'AYE' : 'NAY'
+				});
+				console.log(r);
+			})
+		}
+	}
 }
 
 start().catch(console.error)
+
+
