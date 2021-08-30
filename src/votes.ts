25e45c10ce3dc3ffcee858ec2193f81022cfb8cf
Nikhil Ranjan <niklabh811@gmail.com>
2021-08-30 10:44:32 +0200

    fix

--- src/votes.ts
+++ src/votes.ts
@@ -30,17 +30,16 @@ async function start() {
 			"referendum_index": i
 		};
 
-		const result = await fetch(`https://${NETWORK}.subscan.io/api/scan/democracy/votes`, {
+		const result = await fetch(`https://${NETWORK}.webapi.subscan.io/api/scan/democracy/votes`, {
 			"headers": {
 				"accept": "application/json, text/plain, */*",
 				"accept-language": "en",
 				"content-type": "application/json;charset=UTF-8",
-				"sec-ch-ua": "\"Google Chrome\";v=\"89\", \"Chromium\";v=\"89\", \";Not A Brand\";v=\"99\"",
+				"sec-ch-ua": "\"Chromium\";v=\"92\", \" Not A;Brand\";v=\"99\", \"Google Chrome\";v=\"92\"",
 				"sec-ch-ua-mobile": "?0",
 				"sec-fetch-dest": "empty",
 				"sec-fetch-mode": "cors",
-				"sec-fetch-site": "same-origin",
-				"cookie": "_ga=GA1.2.373893696.1615810717; __gads=ID=719a8ea82f697fe1-22f2d006c8ba0095:T=1615810718:RT=1615810718:S=ALNI_Maikj6BaNzar0UggfrRoUNB-4cQzg; banner=true; __cfduid=d729b35ad5eae5a6c9af7f724dccd64191618817702; local_language=en; _gid=GA1.2.943938073.1618899540; _gat_UA1525613143=1; _gat_UA1525613147=1"
+				"sec-fetch-site": "same-site"
 			},
 			"body": JSON.stringify(body),
 			"method": "POST",
@@ -93,17 +92,16 @@ async function start() {
 			"proposal_id":i
 		};
 
-		const result = await fetch(`https://${NETWORK}.subscan.io/api/scan/council/proposal`, {
+		const result = await fetch(`https://${NETWORK}.webapi.subscan.io/api/scan/council/proposal`, {
 			"headers": {
-			  "accept": "application/json, text/plain, */*",
-			  "accept-language": "en",
-			  "content-type": "application/json;charset=UTF-8",
-			  "sec-ch-ua": "\"Google Chrome\";v=\"89\", \"Chromium\";v=\"89\", \";Not A Brand\";v=\"99\"",
-			  "sec-ch-ua-mobile": "?0",
-			  "sec-fetch-dest": "empty",
-			  "sec-fetch-mode": "cors",
-			  "sec-fetch-site": "same-origin",
-			  "cookie": "__gads=ID=719a8ea82f697fe1-22f2d006c8ba0095:T=1615810718:RT=1615810718:S=ALNI_Maikj6BaNzar0UggfrRoUNB-4cQzg; local_language=en; _gid=GA1.2.2056202929.1629200928; banner=true; _gat_UA1525613143=1; _gat_UA1525613147=1; _ga=GA1.1.373893696.1615810717; _ga_4Q4YQW2GZ3=GS1.1.1629290568.16.1.1629290602.0"
+				"accept": "application/json, text/plain, */*",
+				"accept-language": "en",
+				"content-type": "application/json;charset=UTF-8",
+				"sec-ch-ua": "\"Chromium\";v=\"92\", \" Not A;Brand\";v=\"99\", \"Google Chrome\";v=\"92\"",
+				"sec-ch-ua-mobile": "?0",
+				"sec-fetch-dest": "empty",
+				"sec-fetch-mode": "cors",
+				"sec-fetch-site": "same-site"
 			},
 			"body": JSON.stringify(body),
 			"method": "POST",
@@ -144,7 +142,7 @@ async function start() {
 
 		const body = {proposal_id: i};
 
-		const result = await fetch(`https://${NETWORK}.subscan.io/api/scan/techcomm/proposal`, {
+		const result = await fetch(`https://${NETWORK}.webapi.subscan.io/api/scan/techcomm/proposal`, {
 			"headers": {
 				"accept": "application/json, text/plain, */*",
 				"accept-language": "en",
@@ -153,11 +151,10 @@ async function start() {
 				"sec-ch-ua-mobile": "?0",
 				"sec-fetch-dest": "empty",
 				"sec-fetch-mode": "cors",
-				"sec-fetch-site": "same-origin",
-				"cookie": "__gads=ID=719a8ea82f697fe1-22f2d006c8ba0095:T=1615810718:RT=1615810718:S=ALNI_Maikj6BaNzar0UggfrRoUNB-4cQzg; local_language=en; _gid=GA1.2.2056202929.1629200928; banner=true; _gat_UA1525613143=1; _gat_UA1525613147=1; _ga=GA1.1.373893696.1615810717; _ga_4Q4YQW2GZ3=GS1.1.1629290568.16.1.1629290602.0"
+				"sec-fetch-site": "same-site"
 			},
-			"body": JSON.stringify(body),
-			"method": "POST",
+			"body":  JSON.stringify(body),
+			"method": "POST"
 		});
 
 		const json = await result.json();
